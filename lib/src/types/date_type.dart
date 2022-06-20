8c18dfb5697be61bd0aa302f66a4d230bebd021e
caijinglong-b-imac <cjl_spy@163.com>
2022-06-20 14:31:40 +0800

    doc: date_type

--- lib/src/types/date_type.dart
+++ lib/src/types/date_type.dart
@@ -1,5 +1,11 @@
+/// The unit of the timestamp.
 enum TimestampUnit {
+  /// The unit of the timestamp is seconds.
   seconds,
+
+  /// The unit of the timestamp is milliseconds.
   milliseconds,
+
+  /// The unit of the timestamp is microseconds.
   microseconds,
 }
